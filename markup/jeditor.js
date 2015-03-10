66696ae1f1148eb7127c145a0fde1dd1f752fcc2
unknown <cs062jp@LRQ00103681.diti.lr.net>
2015-03-10 15:03:27 +0000

    jkdjk

--- markup/jeditor.js
+++ markup/jeditor.js
@@ -1,80 +1,85 @@
 	
 
-	var html;
+ 
 	var xxhtml;
-	var json;
-	var rend;
-
+ 
 		
 	function getHTML(){
 		editor.preview()
 		html = editor.getElement('previewer').body.innerHTML;
+ 		editor.edit();
+
 		var rstart = html.replace('<div id="epiceditor-preview">',"");
 		html = rstart.replace(new RegExp('</div>(?!</div>)','g'),"");
-		
-		var ehtml1 = html.replace(new RegExp('\<','g'),"&lt");
-		var ehtml2 = ehtml1.replace(new RegExp('\&','g'),"&amp");
 		var xhtml = html.replace(new RegExp('\"','g'),'\\"');		
-		xxhtml = xhtml.replace (new RegExp('\[\x0A\x0D]','g'),"");
-		
-		
-		disp = "<pre >" + ehtml2 + "</pre>";
-		document.getElementById("html").innerHTML = disp;
- 
-		editor.edit();
+	
+		var disp = xhtml.replace(new RegExp('\&','g'),"&amp");	
+		var xdisp = disp.replace(new RegExp('\<','g'),"&lt");
+		xxdisp = "<pre >" + xdisp + "</pre>";
+		document.getElementById("html").innerHTML =  xxdisp;		
+		return xhtml;
+	}
+	
+	function getMarkup(){
+		var mup = editor.exportFile("epiceditor","text");
+		var xmup = mup.replace(new RegExp('\"','g'),'\\"');	
+		var xxmup = xmup.replace(new RegExp('\[\x0A\x0D]','g'),"");
+		return xxmup;
 	}
 
 	function getJSON(){
+		var xhtml = getHTML();
+		html = xhtml.replace (new RegExp('\[\x0A\x0D]','g'),"");
 		var json;
 		json = '{"article":{';
 		/*							 */
 			json += '"_boost": {"name" : "sponsor", "null_value" : 1.0},';	
 			json += '"properties":{'
 
-			json += '"id":"' + document.getElementById("UID").innerHTML +'",';
-
+			json += '"id":"' + document.getElementById("UID").value +'",';
+			json += '"title":"' + document.getElementById("TITLE").value +'",';
 			json += '"scope":"' + document.getElementById("SCOPE").innerHTML +'",';
-
-
-			json += '"content":"' +  xxhtml + '"';
+			json += '"content":"' +  html + '",';
+			json += '"markup":"' + getMarkup() + '"';
 			json += '}';		
 			json += '}';
 		json += '}';
-		
+		 
 		try{
 			jsonObj= JSON.parse(json);
-			json = "<pre >" + JSON.stringify(jsonObj,null,2) + "</pre>";
-			document.getElementById("json").innerHTML = json;
-			document.getElementById("mjson").value =  json;
+			json = JSON.stringify(jsonObj,null,2) 
+			document.getElementById("json").innerHTML = "<pre >" + json+ "</pre>";
+			return json;
 		}
 		catch(err){
-			document.getElementById("json").innerHTML = "Error";
+			document.getElementById("json").innerHTML = "<p> Error </p> <p>" + err.message + "</p> <pre >" + json+ "</pre>";
+			return "Error creating json file:" + err.message;
 		}
  	}
 	
 	function getCSV(){
 		var csv;
-		csv  = '"' + document.getElementById("UID").innerHTML + '","' ;
+		csv  = '"' + document.getElementById("UID").value + '","' ;
 		csv += document.getElementById("SCOPE").innerHTML + '","' ;
+		csv += document.getElementById("TITLE").value + '","' ;
 		csv += xxhtml + '"';
 		document.getElementById("csv").innerHTML  =csv;
-		document.getElementById("mcsv").value =csv;
+		return csv;
  	}
 
 	function getit(){
-		getHTML();
+		
 		getJSON();
-		getCSV();
+		getCSV(); 
+
 	}
 	
 /*
 */
 	function saveTextAsFile(){
-		getHTML();
-		getJSON();
-		var textToWrite = var json;
+		var textToWrite = getJSON();
 		var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-		var fileNameToSaveAs = "tmp.json";
+		var fileNameToSaveAs = document.getElementById("UID").value+ "_" + document.getElementById("TITLE").value +".json";
 
 		var downloadLink = document.createElement("a");
 		downloadLink.download = fileNameToSaveAs;
@@ -102,6 +107,23 @@
 	document.body.removeChild(event.target);
 }
 
+	function loadJSON(json){
+	try{
+			jsonObj= JSON.parse(json);
+			document.getElementById("json").innerHTML = json;
+			document.getElementById("UID").value=jsonObj.article.properties.id;
+			document.getElementById("TITLE").value=jsonObj.article.properties.title;
+			document.getElementById("SCOPE").innerHTML=jsonObj.article.properties.scope;
+ 
+			document.getElementById('html').innerHTML=jsonObj.article.properties.content;
+			editor.importFile("tmp",jsonObj.article.properties.markup);
+		}
+		catch(err){
+			document.getElementById("json").innerHTML = "Error"+ err.message;
+		}	
+	}
+
+
 	function loadFileAsText()
 	{
 		var fileToLoad = document.getElementById("fileToLoad").files[0];
@@ -109,23 +131,19 @@
 		var fileReader = new FileReader();
 		fileReader.onload = function(fileLoadedEvent) 
 		{
-			var textFromFileLoaded = fileLoadedEvent.target.result;
-			document.getElementById("json").innerHTML = textFromFileLoaded;
+			var textFromFileLoaded = fileLoadedEvent.target.result;		
+ 			loadJSON(textFromFileLoaded);
+			
 		};
 		fileReader.readAsText(fileToLoad, "UTF-8");
 	}
 	
 
-
 	document.onload = function init(){
-	 
 		function showFile(){
 		document.getElementById("filename").value= document.getElementById("fileToLoad").files[0].name;
 		}
-	
 		document.getElementById("fileToLoad").addEventListener("change",showFile);
-		
-		
 	}
 
  
