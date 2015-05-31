c94e3f6f8e379173232b69b5af1fd76f805fc944
curiouscrab <john@curiouscrab.com>
2015-05-31 20:07:03 +0100

    preveiw

--- markup/jexport.js
+++ markup/jexport.js
@@ -81,8 +81,43 @@
 		editor.importFile("epiceditor",mup);		
 	}
 	
-	
-	function preview(){
+	function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removecChild(form);
+}
+    function preview(){
+ 
+ 
+        
+        url = "http://localhost:5001/preview"
+        
+        data = { "kmj" : JSON.stringify(getJSON(),null,2) } 
+        
+        post(url, data) 
+        
+    }
+    
+	function previewxx(){
 		var jsonObj = getJSON();
 		putHTML(jsonObj.content);
 	}
